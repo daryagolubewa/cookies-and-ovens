1092097ffe106f2f40b82159e75dc97b64b62d58
daryagolubeva <darya.golubewa@gmail.com>
2018-10-17 17:04:21 +0300

    method makeCookies() added to class Oven();

--- cookies.js
+++ cookies.js
@@ -1,62 +1,81 @@
-class Cookie {
-	constructor() {
-		this.form = '';
-		this.size = '';
-		this.filling = '';
-		this.timing = 0;
-	}
 
-	static makeDoughCookies(cookiesNumber) {
-		let cookies = [];
-		if(cookiesNumber <= 50) {
-			for (let i = 0; i <= cookiesNumber; i++) {
-				cookies.push(new this());
-			}
-		} else {
-			throw Error('Too many cookies! The number of cookies should be under 50');
-		}
-		return cookies;
-	}
-}
+    class Cookie {
+        constructor() {
+            this.form = '';
+            this.size = '';
+            this.filling = '';
+            this.timing = 0;
+            this.status = 'doughy'
+        }
 
-class ChocolateCookie extends Cookie {
-	constructor() {
-		this.form = 'round';
-		this.size = 'medium'
-		this.filling = 'chocolate';
-		this.timing = 30;
-	}
-}
+        static makeDoughCookies(cookiesNumber) {
+            let doughCookies = [];
+            if(cookiesNumber <= 50) {
+                for (let i = 1; i <= cookiesNumber; i++) {
+                    doughCookies.push(new this());
+                }
+            } else {
+                throw Error('Too many cookies! The number of cookies should be under 50');
+            }
+            return doughCookies;
+        }
+    }
 
-class AlmondCookie extends Cookie {
-	constructor() {
-		this.form = 'square';
-		this.size = 'big'
-		this.filling = 'almond';
-		this.timing = 40;
-	}
-}
+    class ChocolateCookie extends Cookie {
+        constructor() {
+            super()
+            this.form = 'round';
+            this.size = 'medium'
+            this.filling = 'chocolate';
+            this.timing = 30;
+        }
+    }
 
-class kurabieCookie extends Cookie {
-	constructor() {
-		this.form = 'star';
-		this.size = 'small'
-		this.filling = 'jam';
-		this.timing = 50;
-	}
-}
+    class AlmondCookie extends Cookie {
+        constructor() {
+            super()
+            this.form = 'square';
+            this.size = 'big'
+            this.filling = 'almond';
+            this.timing = 40;
+        }
+    }
 
+    class kurabieCookie extends Cookie {
+        constructor() {
+            super()
 
-class Oven {
-
-makeCookies(cookies) {
-		//status
-	}
-
-}
+            this.form = 'star';
+            this.size = 'small'
+            this.filling = 'jam';
+            this.timing = 50;
+        }
+    }
 
 
-let oven = new Oven()
-oven.makeCookies();
+    class Oven {
+        makeCookies(cookies, timing) {
+
+
+                for (let i = 0; i <= timing; i++) {
+                    console.log(cookies[i].timing)
+                    if (cookies[i].timing === timing) {
+                        return cookies[i].status = 'ready'
+                    }
+                    if (cookies[i].timing > timing) {
+                        return cookies[i].status = 'doughy'
+                    }
+                    if (cookies[i].timing < timing) {
+                        return cookies[i].status = 'burned'
+                    }
+                }
+        }
+    }
+
+
+    let oven = new Oven();
+    let cookies = AlmondCookie.makeDoughCookies(30);
+    console.log(cookies);
+    console.log(oven.makeCookies(cookies, 40));
 
 
